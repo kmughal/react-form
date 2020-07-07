421f5e6b4c97078f2b7b1ce61084f9060c61fbd4
Khurram <khurramshahzad@tfl.gov.uk>
2020-07-07 14:12:51 +0100

    fix validation summary display issue for undefined

--- src/components/basic-components/ValidationError/index.tsx
+++ src/components/basic-components/ValidationError/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const ValidationError = (props) => {
-  const valid = props.valid
+  const valid = props.valid === undefined ? true : props.valid
   const message = props.message ?? "Something went wrong"
   return !valid && <p>{message} </p>
 }
