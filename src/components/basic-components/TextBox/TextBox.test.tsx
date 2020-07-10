df2f31ee0be8501dc4fcb937e3f9319cfe1bac5d
Khurram <khurramshahzad@tfl.gov.uk>
2020-07-10 13:05:08 +0100

    test(Textbox): add some more tests for textbox

--- src/components/basic-components/TextBox/TextBox.test.tsx
+++ src/components/basic-components/TextBox/TextBox.test.tsx
@@ -3,24 +3,67 @@ import { BaseComponentProps, TextBox } from ".."
 import render from "../../../../bin/render"
 
 describe("TextBox tests", () => {
-  it("when title, id, name, placeholder and value is set then it should be set on the Textbox", () => {
+  it("when title, id, name, placeholder & type must be text", () => {
     const textBoxProps: BaseComponentProps = {
       id: "txt_name",
       name: "name",
       placeholder: "placeholder",
-      label: "Name :"
+      label: "Name :",
     }
-    const { getById, getByTagName, typeElementText } = render(
+    const { getById, typeElementText } = render(
       <TextBox textBoxProps={textBoxProps} />
     )
 
     const nameTextBox = getById("txt_name")
     expect(nameTextBox).mustHaveAnAttribute("name")
     expect(nameTextBox).mustHaveAnAttribute("placeholder")
+    expect(nameTextBox).typeMustBe("text")
     typeElementText(nameTextBox, "khurram")
     expect(nameTextBox).valueMustBeEqual("khurram")
+  })
+
+  it("must have a label", () => {
+    const textBoxProps: BaseComponentProps = {
+      id: "txt_name",
+      name: "name",
+      placeholder: "placeholder",
+      label: "Name :",
+    }
+    const { getByTagName } = render(<TextBox textBoxProps={textBoxProps} />)
 
     const label = getByTagName("label")
     expect(label).attributeValueMustBeSame("for", "name")
+  })
+
+  describe(" valid flag ", () => {
+    it("when set to true then we are not expecting validation message", () => {
+      const textBoxProps: BaseComponentProps = {
+        id: "txt_name",
+        name: "name",
+        placeholder: "placeholder",
+        label: "Name :",
+        valid: true,
+      }
+      const { getByTagName } = render(<TextBox textBoxProps={textBoxProps} />)
+
+      const validationParagraphMessage = getByTagName("p")
+      expect(validationParagraphMessage).toBeNull()
+    })
+
+    it("when set to false then we are expecting validation message", () => {
+      const textBoxProps: BaseComponentProps = {
+        id: "txt_name",
+        name: "name",
+        placeholder: "placeholder",
+        label: "Name :",
+        valid: false,
+        validationMessage: "Name is not valid",
+      }
+      const { getByTagName } = render(<TextBox textBoxProps={textBoxProps} />)
+
+      const validationParagraphMessage = getByTagName("p")
+      expect(validationParagraphMessage).not.toBeNull()
+      expect(validationParagraphMessage).textContentEqual("Name is not valid")
+    })
   })
 })
