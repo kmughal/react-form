e5da761d85f26ceb96d15442151fba862b6b79bf
Khurram <khurramshahzad@tfl.gov.uk>
2020-07-13 15:48:54 +0100

    add a unit test for checkbox

--- src/components/basic-components/CheckBox/CheckBox.test.tsx
+++ src/components/basic-components/CheckBox/CheckBox.test.tsx
@@ -1,26 +1,41 @@
 import * as React from "react"
-import { BaseComponentProps, CheckBox } from ".."
+import { CheckBoxProps } from "."
+import { CheckBox } from ".."
 import render from "../../../../bin/render"
 
 describe("CheckBox tests", () => {
   it("when title, id, name, placeholder & type must be number", () => {
-    const props: BaseComponentProps = {
+    const props: CheckBoxProps = {
       id: "txt-CheckBox",
       name: "txt-CheckBox",
       placeholder: "Select",
       label: "CheckBox :",
     }
-    const { getById } = render(
-      <CheckBox checkBoxProps={props} />
-    )
+    const { getById } = render(<CheckBox checkBoxProps={props} />)
 
     const CheckBoxBox = getById("txt-CheckBox")
     expect(CheckBoxBox).mustHaveAnAttribute("name")
     expect(CheckBoxBox).typeMustBe("checkbox")
   })
 
+  it("when checked then it should be marked as selected", () => {
+    const props: CheckBoxProps = {
+      id: "txt-CheckBox",
+      name: "txt-CheckBox",
+      placeholder: "Select",
+      label: "CheckBox :",
+      checked: true,
+    }
+    const { getById } = render(<CheckBox checkBoxProps={props} />)
+
+    const CheckBoxBox = getById("txt-CheckBox")
+    expect(CheckBoxBox).mustHaveAnAttribute("name")
+    expect(CheckBoxBox).typeMustBe("checkbox")
+    expect(CheckBoxBox.checked).toBeTruthy()
+  })
+
   it("must have a label", () => {
-    const props: BaseComponentProps = {
+    const props: CheckBoxProps = {
       id: "txt-CheckBox",
       name: "txt-CheckBox",
       placeholder: "Provide CheckBox",
@@ -34,7 +49,7 @@ describe("CheckBox tests", () => {
 
   describe(" valid flag ", () => {
     it("when set to true then we are not expecting validation message", () => {
-      const props: BaseComponentProps = {
+      const props: CheckBoxProps = {
         id: "txt-CheckBox",
         name: "txt-CheckBox",
         placeholder: "Provide CheckBox",
@@ -48,7 +63,7 @@ describe("CheckBox tests", () => {
     })
 
     it("when set to false then we are expecting validation message", () => {
-      const props: BaseComponentProps = {
+      const props: CheckBoxProps = {
         id: "txt-CheckBox",
         name: "txt-CheckBox",
         placeholder: "Provide CheckBox",
